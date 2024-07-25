cf10e87bccfcb23aadf7871ccf36b4145c444c31
nitaysason <nitaysason@gmail.com>
2024-07-25 13:38:54 +0300

    pictureurl

--- frontend/App-hotel/src/app/app.component.ts
+++ frontend/App-hotel/src/app/app.component.ts
@@ -21,6 +21,15 @@ export class AppComponent implements OnInit {
   isLoggedIn: boolean = false;
   isStaff: boolean = false;
   title = 'App-hotel'; 
+  currentImage: string = '';
+
+  images: string[] = [
+    'http://www.alp.co.il/hotelImage/Pool_19799.jpg',
+    'http://www.alp.co.il/hotelImage/HOTEL1_19799.jpg',
+    'http://www.alp.co.il/hotelImage/lobby2_19799.jpg',
+    'http://www.alp.co.il/hotelImage/Restaurant_19799.jpg'
+  ];
+
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
@@ -28,11 +37,19 @@ export class AppComponent implements OnInit {
       this.isLoggedIn = loggedIn;
       this.isStaff = !!localStorage.getItem('staff_id');
     });
+
+    this.changeBackgroundImage();
+    setInterval(() => this.changeBackgroundImage(), 5000); // Change image every 5 seconds
   }
 
   logout() {
     this.authService.logout().subscribe(() => {
       this.router.navigate(['/login']); // Navigate to login page after logout
     });
+  }
+
+  changeBackgroundImage() {
+    const randomIndex = Math.floor(Math.random() * this.images.length);
+    this.currentImage = this.images[randomIndex];
   }
 }
